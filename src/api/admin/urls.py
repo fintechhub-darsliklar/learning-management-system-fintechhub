76b0e9627558c6e2e31d8c42f6c85e098ad9c1c5
Ziyodullo <ziyodullodev@gmail.com>
2026-05-13 18:40:08 +0500

    asdf

--- src/api/admin/urls.py
+++ src/api/admin/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path, include
-from api.admin.views import students_views, admin_views
-\
+from api.admin.views import students_views
+
 
 urlpatterns = [
     path('student/', students_views.StudentListApiView.as_view()),
     path('student/create/', students_views.StudentCreateApiView.as_view()),
-    path('admin/', admin_views.UserListApiView.as_view()),  
-    path('admin/create/', admin_views.UserCreateApiView.as_view()),
 
 ]
